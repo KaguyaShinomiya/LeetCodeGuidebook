0f04aeda38b69ea8f9570ceb91f7d4b01c86367a
KaguyaShinomiya <1154611508@qq.com>
2021-01-06 01:23:51 +0800

    修改: 归并排序

--- src/排序与查找/经典排序/MergeSort.java
+++ src/排序与查找/经典排序/MergeSort.java
@@ -1,37 +1,33 @@
 package 排序与查找.经典排序;
 
 public class MergeSort {
-    public static void mergeSort(int[] a, int[] tmpArray, int left, int right) {
-        int center;
-        //int TR2[];
-
+    public static void mergeSort(int[] arr, int[] tmpArray, int left, int right) {
         if (left < right) {
-            center = (left + right) / 2;
-            mergeSort(a, tmpArray, left, center);
-            mergeSort(a, tmpArray, center + 1, right);
-            merge(a, tmpArray, left, center + 1, right);
+            int mid = (left + right) / 2;
+            mergeSort(arr, tmpArray, left, mid);
+            mergeSort(arr, tmpArray, mid + 1, right);
+            merge(arr, tmpArray, left, mid + 1, right);
         } else {
-            tmpArray[left] = a[left];
+            tmpArray[left] = arr[left];
         }
 
     }
 
-    public static void merge(int[] a, int[] tmpArray, int leftPos, int rightPos, int rightEnd) {
+    public static void merge(int[] arr, int[] tmpArray, int leftPos, int rightPos, int rightEnd) {
         int leftEnd = rightPos - 1;
         int tempPos = leftPos;
-        int numElements = rightEnd - leftPos + 1;
         while (leftPos <= leftEnd && rightPos <= rightEnd) {
-            if (a[leftPos] <= a[rightPos]) {
-                tmpArray[tempPos++] = a[leftPos++];
+            if (arr[leftPos] <= arr[rightPos]) {
+                tmpArray[tempPos++] = arr[leftPos++];
             } else {
-                tmpArray[tempPos++] = a[rightPos++];
+                tmpArray[tempPos++] = arr[rightPos++];
             }
 
             while (leftPos < leftEnd) {
-                tmpArray[tempPos++] = a[leftPos++];
+                tmpArray[tempPos++] = arr[leftPos++];
             }
             while (rightPos < rightEnd) {
-                tmpArray[tempPos++] = a[rightPos++];
+                tmpArray[tempPos++] = arr[rightPos++];
             }
         }
     }
